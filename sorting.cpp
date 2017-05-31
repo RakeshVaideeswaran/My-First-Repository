b8136d2067e869d13c3b087c2f4bf082243a9287
Rakesh Vaideeswaran <raku18998@gmail.com>
2017-05-31 16:16:09 +0530

    Added sorting.cpp file

---
+++ sorting.cpp
@@ -0,0 +1,26 @@
+//BUBBLE SORT - ASCENDING ORDER
+
+#include<stdio.h>
+
+void main()
+{
+	int n,a[20],i,j,temp;
+	
+	cin>>n;
+	
+	for(i=0;i<n;i++)
+	cin>>a[i];
+	
+	for(i=0;i<n;i++)
+	for(j=0;j<n-i-1;j++)
+	 if(a[j+1]<a[j])
+	 {
+	 	temp=a[j];
+	 	a[j]=a[j+1];
+	 	a[j+1]=temp;
+	 }
+	 
+	 for(i=0;i<n;i++)
+	 cout<<a[i]<<" ";
+	
+}
